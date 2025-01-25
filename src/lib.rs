721d0ec5ae5f72947c8370fa75b1b4b2f219c9f5
RobertasJ <robertasj8@gmail.com>
2025-01-25 16:58:12 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{env, fs::read_to_string, path::PathBuf};
 
 use quote::quote;
 use syn::{parse_macro_input, LitStr};
@@ -12,6 +12,10 @@ pub fn inline_doc(
 
     let path = parse_macro_input!(attr as LitStr);
     let path = path.value();
+    let cargo_manifest =
+        env::var("$CARGO_MANIFEST_DIR").expect("to find the $CARGO_MANIFEST_DIR env variable");
+    let cargo_manifest = PathBuf::from(cargo_manifest);
+    let path = cargo_manifest.join(path);
 
     let md = read_to_string(&path).unwrap_or_else(|_| panic!("Could not find {path:?}"));
     let md = format!("\n\n{}\n\n\n", md.trim());
